23aa88d147c7953cd71a9f33a663e2fee9cb2a9b
ssong6735 <because6735@gmail.com>
2021-06-11 15:59:44 +0900

    [21.06.11] # kakao login api - 토큰 발급까지

---
+++ src/main/java/com/spring/mvc/web/member/domain/OAuthValue.java
@@ -0,0 +1,10 @@
+package com.spring.mvc.web.member.domain;
+
+// 소셜로그인 관련 상수
+public interface OAuthValue {
+    //REST API 키	22b976b3af1ca5cff211d7f333083a25
+    //Redirect URI	http://localhost:8181/auth/kakao
+    String KAKAO_APP_KEY = "22b976b3af1ca5cff211d7f333083a25";
+    String KAKAO_REDIRECT_URI = "/auth/kakao";
+
+}
